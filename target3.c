a92b96369c55f696c1b3a5b6b70cfad570ac2ae3
giupradop <134300550+giupradop@users.noreply.github.com>
2024-10-05 17:29:27 -0300

    Update target3.c

--- target3.c
+++ target3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int INDICE = 12, SOMA = 0, K = 0;
+    int INDICE = 12, SOMA = 0, K = 1;
     while (K < INDICE) {
         K = K + 1;
         SOMA = SOMA + K;
